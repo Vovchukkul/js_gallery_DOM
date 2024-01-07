3805bd443e25e64831d0edb7f8ff9e4317475c36
Vovchuk <kulivackijvoloda@gmail.com>
2024-01-07 20:49:21 +0200

    make build

---
+++ index.09dd0ffa.js
@@ -0,0 +1,3 @@
+/* eslint-disable no-shadow */var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");if(r){var c=r.getAttribute("href");e.src=c}});//# sourceMappingURL=index.09dd0ffa.js.map
+
+//# sourceMappingURL=index.09dd0ffa.js.map
